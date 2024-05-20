6f205d0f8f8ac33d23e9485ea12edbfd292e5d91
Yucheol Shin <brongersmai2@gmail.com>
2024-05-20 19:39:24 +0900

    add codes

---
+++ codes/test_CCVI.R
@@ -0,0 +1,10 @@
+###  follow the tutorial
+library(ccviR)
+
+# launch the vignette
+vignette('app_vignette', package = 'ccviR')
+vignette('app_details_vignette', package = 'ccviR')   # using app in practice
+vignette('data_prep_vignette', package = 'ccviR')     # prepare custom climate data
+vignette('package_vignette', package = 'ccviR')       # directly in R
+
+
